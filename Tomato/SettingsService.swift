b2ae061fd5c3fcec0f234a5c7bbff29c35eec7c1
Damon Cricket <d.a.tsvirko@gmail.com>
2020-01-23 19:36:55 +0200

    Add settings view controller.

--- Tomato/SettingsService.swift
+++ Tomato/SettingsService.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+
+class SettingsService {
+    struct Coding {
+        static let pomodoroTime = "pomodoro.time.coding.constant"
+        static let breakTime = "break.time.coding.constant"
+        static let longBreakTime = "long.break.time.coding.constant"
+        static let longBreakEach = "long.break.each.coding.constant"
+    }
+    struct Constants {
+        static let pomodoroTime = 30
+        static let breakTime = 5
+        static let longBreakTime = 10
+        static let longBreakEach = 3
+    }
+    
+    static let shared = SettingsService()
+    var pomodoroTime: Int {
+        set {
+            UserDefaults.standard.set(newValue, forKey: Coding.pomodoroTime)
+        } get {
+            return UserDefaults.standard.integerValue(forKey: Coding.pomodoroTime, defaultValue: Constants.pomodoroTime)
+        }
+    }
+    var breakTime: Int {
+        set {
+            UserDefaults.standard.set(newValue, forKey: Coding.breakTime)
+        } get {
+            return UserDefaults.standard.integerValue(forKey: Coding.breakTime, defaultValue: Constants.breakTime)
+        }
+    }
+    var longBreakTime: Int {
+        set {
+            UserDefaults.standard.set(newValue, forKey: Coding.longBreakTime)
+        } get {
+            return UserDefaults.standard.integerValue(forKey: Coding.longBreakTime, defaultValue: Constants.longBreakTime)
+        }
+    }
+    var longBreakEach: Int {
+        set {
+            UserDefaults.standard.set(newValue, forKey: Coding.longBreakEach)
+        } get {
+            return UserDefaults.standard.integerValue(forKey: Coding.longBreakEach, defaultValue: Constants.longBreakEach)
+        }
+    }
+}
+
+extension UserDefaults {
+    func integerValue(forKey key: String, defaultValue: Int) -> Int {
+        if let number = UserDefaults.standard.object(forKey: key) as? NSNumber {
+            return number.intValue
+        } else {
+            return defaultValue
+        }
+    }
+}
--- Tomato/SettingsViewController.swift
+++ Tomato/SettingsViewController.swift
@@ -8,23 +8,53 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var pomodoroTimeTextField: UITextField?
+    @IBOutlet weak var breakTimeTextField: UITextField?
+    @IBOutlet weak var longBreakTimeTextField: UITextField?
+    @IBOutlet weak var longBreakEachTextField: UITextField?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let settings = SettingsService.shared
+        pomodoroTimeTextField?.text = "\(settings.pomodoroTime)"
+        breakTimeTextField?.text = "\(settings.breakTime)"
+        longBreakTimeTextField?.text = "\(settings.longBreakTime)"
+        longBreakEachTextField?.text = "\(settings.longBreakEach)"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        let settings = SettingsService.shared
+        settings.pomodoroTime = pomodoroTimeTextField!.integerValue
+        settings.breakTime = breakTimeTextField!.integerValue
+        settings.longBreakTime = longBreakTimeTextField!.integerValue
+        settings.longBreakEach = longBreakEachTextField!.integerValue
+        super.viewWillDisappear(animated)
     }
-    */
+    
+    @IBAction func closeButtonTap(sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let text = textField.text, text.isEmpty {
+            let settings = SettingsService.shared
+            if textField == pomodoroTimeTextField {
+                textField.text = "\(settings.pomodoroTime)"
+            } else if textField == breakTimeTextField {
+                textField.text = "\(settings.breakTime)"
+            } else if textField == longBreakTimeTextField {
+                textField.text = "\(settings.longBreakTime)"
+            } else if textField == longBreakEachTextField {
+                textField.text = "\(settings.longBreakEach)"
+            }
+        }
+    }
+}
 
+extension UITextField {
+    var integerValue: Int {
+        return Int(text != nil ? text! : "0")!
+    }
 }
