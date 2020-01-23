b33135b73b2f787d13d50bd1ff42b51e8ee372ae
Damon Cricket <d.a.tsvirko@gmail.com>
2020-01-23 22:43:08 +0200

    Add breaks and long breaks.

--- Tomato/MainViewController.swift
+++ Tomato/MainViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    enum State {
+        case pomodoro
+        case space
+        case longSpace
+    }
     @IBOutlet weak var ringView: RingView?
     @IBOutlet weak var percentView: PercentView?
     @IBOutlet weak var timerLabel: UILabel?
@@ -16,6 +21,12 @@ class MainViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton?
     var timer: Timer? = nil
     var time: Int = 0
+    var pomodoros: Int = 0
+    var state: State = .pomodoro
+    
+    deinit {
+        stopTimer()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +53,8 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func startButtonTap(sender: UIButton) {
+        pomodoros = 0
+        state = .pomodoro
         if timer == nil {
             timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerTick(timer:)), userInfo: nil, repeats: true)
             startButton?.setImage(UIImage(named: "stop"), for: .normal)
@@ -54,13 +67,35 @@ class MainViewController: UIViewController {
     
     @objc func timerTick(timer: Timer) {
         time += 1
-        var seconds = SettingsService.shared.pomodoroTime - time
+        let settings = SettingsService.shared
+        var seconds = 0
+        switch state {
+        case  .pomodoro: seconds = settings.pomodoroTime - time
+        case     .space: seconds = settings.breakTime - time
+        case .longSpace: seconds = settings.longBreakTime - time
+        }
         let minutes = seconds/60
         seconds -= minutes*60
         timerLabel?.text = "\(minutes):\(seconds)"
         if seconds == 0 {
-            stopTimer()
-            timerLabel?.text = "\(SettingsService.shared.pomodoroTime/60):00"
+            time = 0
+            switch state {
+            case  .pomodoro:
+                pomodoros += 1
+                if pomodoros%settings.longBreakEach == 0 {
+                    state = .longSpace
+                    timerLabel?.text = "\(settings.longBreakTime/60):00"
+                } else {
+                    state = .space
+                    timerLabel?.text = "\(settings.breakTime/60):00"
+                }
+            case     .space:
+                state = .pomodoro
+                timerLabel?.text = "\(settings.pomodoroTime/60):00"
+            case .longSpace:
+                state = .pomodoro
+                timerLabel?.text = "\(settings.pomodoroTime/60):00"
+            }
         }
     }
     
